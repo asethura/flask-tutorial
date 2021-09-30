c0b8a5d4a800579ae2f2e08c7f9a5b5b3f612d7b
ashoksirpi <89169284+ashoksirpi@users.noreply.github.com>
2021-09-29 23:33:09 -0500

    heroku

--- api-tests/apitest.py
+++ api-tests/apitest.py
@@ -3,6 +3,12 @@ import json
 
 host_url = "https://guarded-scrubland-59466.herokuapp.com/"
 
-response = requests.get(host_url+"Ashok")
-result = response.data.decode('utf-8')
-assert result == 'Hello Ashok'
+
+def test_hello_name():
+    response = requests.get(host_url+"Ashok")
+
+    print(response.text)
+
+    result = response.text
+
+    assert result == 'Hello Ashok'
